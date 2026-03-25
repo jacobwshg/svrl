1193e6585d830c027ef31fe0142d987b381769a2
jacobwshg <wsg438@gmail.com>
2026-03-24 22:05:31 -0500

    added quantization helpers

---
+++ sv/quant_pkg.sv
@@ -0,0 +1,29 @@
+
+package quant_pkg;
+
+	import globals_pkg::*;
+
+	function automatic logic signed [ DWIDTH-1:0 ]
+	QUANT( input logic signed [ DWIDTH-1:0 ] x );
+
+		return $signed( x << FRAC_WIDTH );
+
+	endfunction
+
+	function automatic logic signed [ DWIDTH-1:0 ]
+	DEQUANT( input logic signed [ DWIDTH-1:0 ] x );
+
+		logic signed [ DWIDTH-1:0 ] dq = $signed( x ) >>> FRAC_WIDTH;
+		// add 1 if x both is negative and 
+		// has 1 in the fractional bits
+		if ( x[ DWIDTH-1 ] && ( | x[ FRAC_WIDTH-1:0 ] ) )
+		begin
+			++dq;
+		end
+
+		return dq;
+
+	endfunction
+
+endpackage: quant_pkg
+
